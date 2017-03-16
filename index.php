1e2db4eb7d71318cc04e4d8d58003556846be7d3
pantomimecow <subscriptions@pantomimecow.co.uk>
2017-03-16 15:17:49 +0000

    Initial commit
    Boilerplate of index.php (creation)

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>Marpatt - Kitchen Doors Suppliers to the trade</title>
+	<link rel="stylesheet" href="css/style.css">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	
+	<!--[if IE]>
+		<script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+	<![endif]-->
+</head>
+
+<body id="home">
+
+	
+
+</body>
+</html>
