1ba0a737452e6b80dc50b712842aa5619844abe2
pantomimecow <subscriptions@pantomimecow.co.uk>
2017-04-05 16:21:42 +0100

    Infinite Collection done

---
+++ foundation-range.php
@@ -0,0 +1,39 @@
+
+		<?php include('includes/header.php');?>
+		
+		<img src="img/woodsman.jpg" alt="">
+		<section class="content news about">
+			<div class="gridText">
+			<article id="about_text">
+				<h2>foundation range</h2>
+				<p>The infinte collection is made up of four individual ranges. You can select from a range of beautiful standard colours or optionally we can match accurately to any paint colour or code, meaning your customers don&rsquo; have to compromise on the kitchen of their dreams. All of our kitchens are made with passion and acute attention to detail using the finest materials available.</p>
+			</article>
+			<img src="img/shaker_nero_300_3.jpeg" alt="" id="range">
+			<article id="values" class="fourth">
+				<h3>classic collection</h3>
+				<img src="img/accent.jpg">
+				<p>These kitchens have been with passion and acute attention to detail using the finest materials available, all sourced from sustainable forests. Subtle innovative touches can be seen in the wide vairety of classic and contemporary kitchens we offer.</p>
+				<p>We strive for excellence in everything we do, from construction through to the multitude of different finishes avaialble.</p>
+			</article>
+			<article class="fourth">
+				<h3>contemporary collection</h3>
+				<img src="img/horizon.jpg">
+				<p>Marpatt has added some contemporary designs to compliment its established product range. So we can now offer everything form European High Gloss Matt Painted and Wood Slab and Handless Doors to stunning Wood and Painted Kitchens in many different designs</p>
+			</article>
+			<article class="fourth clearLeft">
+				<h3>edwardian colleciton</h3>
+				<img src="img/edwardian.jpg">
+				<p>These beaded models are a quintessential cornerstone of British design, available with a contemporary &lsquo;Bevelled&rsquo; design or a more traditional &lsquo;Craft&rsquo; desgin. With kitchens getting larger and represeting more of the total living space of your home, the extra detail of beading adds some welcome individuality to the popular Regent and Mowbray shaker designs.</p>
+			</article>
+			<article class="fourth">
+				<h3>bespoke-inframe</h3>
+				<img src="img/stamford.jpg">
+				<p>Within the Bespoke In-frame range every kitchen will be unique, tailored to your exact inidividual requirements. The many styles in the range are available in a choice of solid woods and different finishes. These are complemented by a variety of accessories which will help you style your clients style the kitchen to their own personal taste.</p>
+				<p>The key feature of the bespoke in-frame range is the beautifully constructed door attached to a frame by butt hinges. To secure the door once it is shut we use a brass ball and chatch system.</p>
+			</article>
+			</div>
+
+			
+		
+		</section>
+		<?php include('includes/footer.php');?>
